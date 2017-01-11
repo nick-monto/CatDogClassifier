4b27fbbcd0c70406bce203ccbd79f6118597b439
Nick Monto <monto.nicholas@gmail.com>
2017-01-11 12:12:18 -0500

    Add files via upload

---
+++ ImgPreprocessorKerasIMGEN.py
@@ -0,0 +1,54 @@
+from keras.preprocessing.image import ImageDataGenerator
+
+trainDIR = 'input/trainSmall'  # insert training directory
+saveTrainDIR = 'input/previewTrain'
+
+testDIR = 'input/validation'  # insert test directory
+saveValDIR = 'input/previewVal'
+
+img_width = 100
+img_height = 100
+
+train_datagen = ImageDataGenerator(
+            rotation_range=20,  # random rotation in degrees
+            rescale=1./255,  # rescaling of the pixel information to 0-1 format
+            width_shift_range=0.1,  # percent of random shift, up to 10%
+            height_shift_range=0.1,
+            zoom_range=0.1,  # random percent zoom up to 10%
+            horizontal_flip=True,  # random flipping on the horizontal axis
+            fill_mode='nearest')  # filling of empty pixels
+
+# only color rescale for test set
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+# this generator will read pictures found in a sub folder
+# it will indefinitely generate batches of augmented image data
+train_generator = train_datagen.flow_from_directory(
+        trainDIR,
+        target_size=(img_width, img_height),
+        batch_size=1,
+        class_mode='binary',
+        save_to_dir=saveTrainDIR,
+        save_format='jpeg')
+
+i = 0
+for batch in train_generator:
+    i += 1
+    if i > 20:  # generate twenty images to see what you're creating
+        break
+    print('Done with training image!')
+
+validation_generator = test_datagen.flow_from_directory(
+        testDIR,
+        target_size=(img_width, img_height),
+        batch_size=1,
+        class_mode='binary',
+        save_to_dir=saveValDIR,
+        save_format='jpeg')
+
+i = 0
+for batch in validation_generator:
+    i += 1
+    if i > 20:
+        break
+    print('Done with validation image!')
